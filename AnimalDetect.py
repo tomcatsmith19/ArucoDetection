62c4b7ba25e3ca7da52db07ccf93bd4747695b09
Thomas J. Smith <tomcatsmith19@gmail.com>
2023-06-28 13:18:19 -0500

    Updated testing code between matlab and python

--- AnimalDetect.py
+++ AnimalDetect.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 import math
+import sys
+import os
+import socket
 
 # Define the font for drawing text on the image
 font = cv2.FONT_HERSHEY_PLAIN
@@ -27,43 +30,81 @@ if not cap.isOpened():
 def set_focus_value(value):
     cap.set(cv2.CAP_PROP_FOCUS, value)
 
+# Create a socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Set host to local machine (these values are default... only change port when necessary)
+host = '127.0.0.1'
+port = 9999
+
+# Bind to the port
+serversocket.bind((host, port))
+print('Waiting for connection to MATLAB... You may now start the MATLAB script')
+
+# Queue up to 5 requests
+serversocket.listen(5)
+
+# Establish a connection to the MATLAB script
+clientsocket, addr = serversocket.accept()
+
 while True:
     # Read frame from the camera
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Detect ArUco markers in the frame
-    corners, marker_ids, rejected = cv2.aruco.detectMarkers(frame, dictionary)
+    # Receive the data from MATLAB (0 = do nothing, 1 = begin detection analysis)
+    dataReceived = clientsocket.recv(1024)
 
-    if corners:
-        for corner, marker_id in zip(corners, marker_ids):
-            corner = corner.reshape(4, 2)
-            corner = corner.astype(int)
+    # Print the received data
+    #print("Data received from MATLAB: ", dataReceived)
 
-            top_right, top_left, bottom_right, bottom_left = corner
+    if dataReceived == 1:
+        # Detect ArUco markers in the frame
+        corners, marker_ids, rejected = cv2.aruco.detectMarkers(frame, dictionary)
 
-            # Calculate radian yaw angle of the pose
-            delta_pos = top_left - top_right
-            yaw = math.degrees(np.arctan2(delta_pos[1], delta_pos[0]))
+        if corners:
+            for corner, marker_id in zip(corners, marker_ids):
+                corner = corner.reshape(4, 2)
+                corner = corner.astype(int)
+                
+                top_right, top_left, bottom_right, bottom_left = corner
 
-            # Calculate centroid of the marker
-            centroid = np.mean(corner, axis=0).astype(int)
-            centroid_x = centroid[0]
+                # Calculate radian yaw angle of the pose
+                delta_pos = top_left - top_right
+                yaw = math.degrees(np.arctan2(delta_pos[1], delta_pos[0]))
 
-            # Determine color based on marker orientation and centroid position
-            color = (0, 255, 0) if (45 < yaw < 135 and centroid_x > frame.shape[1] // 2) else (0, 255, 255)
+                # Calculate centroid of the marker
+                centroid = np.mean(corner, axis=0).astype(int)
+                centroid_x = centroid[0]
 
-            # Draw marker outline and label corners
-            cv2.polylines(frame, [corner], True, color, 3, cv2.LINE_AA)
-            cv2.putText(frame, "FL", tuple(top_right), font, 1.3, (255, 0, 255), 2)
-            cv2.putText(frame, "FR", tuple(top_left), font, 1.3, (255, 0, 255), 2)
-            cv2.putText(frame, "BR", tuple(bottom_right), font, 1.3, (255, 0, 255), 2)
-            cv2.putText(frame, "BL", tuple(bottom_left), font, 1.3, (255, 0, 255), 2)
+                # Determine color based on marker orientation and centroid position
+                color = (0, 255, 0)
+                if 45 < yaw < 135 and centroid_x > frame.shape[1] // 2:
+                    # Change the border color of the marker to green (animal was paying attention)
+                    color = (0, 255, 0)
+                    
+                    # Send distraction data to client
+                    msg = "0" # Not distracted
+                    clientsocket.send(msg.encode('ascii'))
+                else:
+                    # Keep border color of the marker at yellow (animal was not paying attention)
+                    color = (0, 255, 255)
 
-            # Print yaw value in the center of the marker
-            yaw_text = f"{int(yaw)} deg"
-            cv2.putText(frame, yaw_text, tuple(centroid), font, 1.3, (0, 0, 255), 2)
+                    # Send distraction data to client
+                    msg = "1" # Distracted
+                    clientsocket.send(msg.encode('ascii'))
+
+                # Draw marker outline and label corners
+                cv2.polylines(frame, [corner], True, color, 3, cv2.LINE_AA)
+                cv2.putText(frame, "FL", tuple(top_right), font, 1.3, (255, 0, 255), 2)
+                cv2.putText(frame, "FR", tuple(top_left), font, 1.3, (255, 0, 255), 2)
+                cv2.putText(frame, "BR", tuple(bottom_right), font, 1.3, (255, 0, 255), 2)
+                cv2.putText(frame, "BL", tuple(bottom_left), font, 1.3, (255, 0, 255), 2)
+
+                # Print yaw value in the center of the marker
+                yaw_text = f"{int(yaw)} deg"
+                cv2.putText(frame, yaw_text, tuple(centroid), font, 1.3, (0, 0, 255), 2)
 
     # Draw ROI outline for the right half of the screen
     roi_start = (frame.shape[1] // 2, 0)
@@ -87,3 +128,6 @@ cap.release()
 
 # Close all OpenCV windows
 cv2.destroyAllWindows()
+
+# Close the client socket
+clientsocket.close()
