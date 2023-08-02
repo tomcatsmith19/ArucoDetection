c43288c1930e1e8976a6872d47afef86ed648d22
Smith <tjs200005@utdallas.edu>
2023-08-02 14:53:26 -0500

    Updates

--- AnimalDetect.py
+++ AnimalDetect.py
@@ -11,14 +11,14 @@ dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 cap = cv2.VideoCapture(2)
 cap.set(cv2.CAP_PROP_FOCUS, 20)
 cap.set(cv2.CAP_PROP_ZOOM, 0)
-cap2 = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(1)
 cap2.set(cv2.CAP_PROP_FOCUS, 20)
 cap2.set(cv2.CAP_PROP_ZOOM, 0)
 print("Cameras connected")
 
 # server socket setup with MATLAB
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 9999))
+s.connect(("localhost", 9995))
 s.setblocking(False)
 print("Connected to local host")
 
@@ -80,13 +80,13 @@ while int(trialStatus) != -1:
                     break
                 
                 # Draw ROI outline for the right half of the screen for camera 1
-                roi_start = ((frame.shape[1] // 2 - 50), 0)
+                roi_start = ((frame.shape[1] // 2), 0)
                 roi_end = (frame.shape[1], frame.shape[0])
                 cv2.rectangle(frame, roi_start, roi_end, (255, 0, 0), 3)
 
                 # Draw ROI outline for the left half of the screen for camera 2
                 roi_start2 = (0, 0)
-                roi_end2 = ((frame.shape[1] // 2) + 50, frame2.shape[0])
+                roi_end2 = ((frame.shape[1] // 2), frame2.shape[0])
                 cv2.rectangle(frame2, roi_start2, roi_end2, (255, 0, 0), 3)
 
                 # Detect ArUco markers in the frames of both cameras
@@ -114,8 +114,7 @@ while int(trialStatus) != -1:
                         cv2.putText(frame, yaw_text, tuple(centroid), font, 1.3, (0, 0, 255), 2)
 
                         # Determine color based on marker orientation and centroid position
-                        color = (0, 255, 0)
-                        if 5 < yaw < 175 and centroid_x > ((frame.shape[1] // 2) - 50):
+                        if 5 < yaw < 175 and centroid_x > ((frame.shape[1] // 2)):
                             # Change the border color of the marker to green (animal was not distracted)
                             color = (0, 255, 0)
                             distraction = 0
@@ -151,8 +150,7 @@ while int(trialStatus) != -1:
                         cv2.putText(frame2, yaw_text2, tuple(centroid2), font, 1.3, (0, 0, 255), 2)
 
                         # Determine color based on marker orientation and centroid position
-                        color2 = (0, 255, 0)
-                        if -175 < yaw2 < -5 and centroid_x2 < ((frame2.shape[1] // 2) + 50):
+                        if -175 < yaw2 < -5 and centroid_x2 < ((frame2.shape[1] // 2)):
                             # Change the border color of the marker to green (animal was not distracted)
                             color2 = (0, 255, 0)
                             distraction = 0
@@ -189,13 +187,13 @@ while int(trialStatus) != -1:
         break
     
     # Draw ROI outline for the right half of the screen for camera 1
-    roi_start = ((frame.shape[1] // 2 - 50), 0)
+    roi_start = ((frame.shape[1] // 2), 0)
     roi_end = (frame.shape[1], frame.shape[0])
     cv2.rectangle(frame, roi_start, roi_end, (255, 0, 0), 3)
 
     # Draw ROI outline for the left half of the screen for camera 2
     roi_start2 = (0, 0)
-    roi_end2 = ((frame.shape[1] // 2) + 50, frame2.shape[0])
+    roi_end2 = ((frame.shape[1] // 2), frame2.shape[0])
     cv2.rectangle(frame2, roi_start2, roi_end2, (255, 0, 0), 3)
 
     # Detect ArUco markers in the frames of both cameras
@@ -223,13 +221,12 @@ while int(trialStatus) != -1:
             cv2.putText(frame, yaw_text, tuple(centroid), font, 1.3, (0, 0, 255), 2)
 
             # Determine color based on marker orientation and centroid position
-            color = (0, 255, 0)
-            if 5 < yaw < 175 and centroid_x > ((frame.shape[1] // 2) - 50):
-                # Change the border color of the marker to green (animal was not distracted)
-                color = (0, 255, 0)
+            if 5 < yaw < 175 and centroid_x > ((frame.shape[1] // 2)):
+                # Change the border color of the marker to violet (animal was not distracted)
+                color = (204, 0, 102)
             else:
                 # Keep border color of the marker at yellow (animal was distracted)
-                color = (0, 255, 255)
+                color = (255, 0, 255)
 
             # Draw marker outline and label corners
             cv2.polylines(frame, [corner], True, color, 3, cv2.LINE_AA)
@@ -259,13 +256,12 @@ while int(trialStatus) != -1:
             cv2.putText(frame2, yaw_text2, tuple(centroid2), font, 1.3, (0, 0, 255), 2)
 
             # Determine color based on marker orientation and centroid position
-            color2 = (0, 255, 0)
-            if -175 < yaw2 < -5 and centroid_x2 < ((frame2.shape[1] // 2) + 50):
+            if -175 < yaw2 < -5 and centroid_x2 < ((frame2.shape[1] // 2)):
                 # Change the border color of the marker to green (animal was not distracted)
-                color2 = (0, 255, 0)
+                color2 = (204, 0, 102)
             else:
                 # Keep border color of the marker at yellow (animal was distracted)
-                color2 = (0, 255, 255)
+                color2 = (255, 0, 255)
 
             # Draw marker outline and label corners
             cv2.polylines(frame2, [corner2], True, color2, 3, cv2.LINE_AA)
